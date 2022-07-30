d8b0f3ccf26af63f22ed8c9ba5d2cc43f24b73cf
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-07-31 00:28:49 +0300

    Update api.py

--- apiProject/utils/api.py
+++ apiProject/utils/api.py
@@ -43,8 +43,40 @@ class Google_maps_api():
     def get_new_place(place_id):
 
         get_resource = "/maps/api/place/get/json"           # Ресурс метода Get
-        get_url = base_url + get_resource + key + "&place_id" + place_id
+        get_url = base_url + get_resource + key + "&place_id=" + place_id
         print(get_url)
-        result_get = Http_method.get(get_url)
+        result_get = Http_methods.get(get_url)
         print(result_get.text)
         return result_get
+
+    """Метод для изменения новой локации"""
+
+    @staticmethod
+    def put_new_place(place_id):
+
+        put_resource = "/maps/api/place/update/json"           # Ресурс метода Put
+        put_url = base_url + put_resource + key
+        print(put_url)
+        json_for_update_new_location = {
+            "place_id": place_id,
+            "address": "100 Lenina street, RU",
+            "key": "qaclick123"
+        }
+        result_put = Http_methods.put(put_url, json_for_update_new_location)
+        print(result_put.text)
+        return result_put
+
+    """Метод для удаления новой локации"""
+
+    @staticmethod
+    def delete_new_place(place_id):
+
+        delete_resource = "/maps/api/place/delete/json"           # Ресурс метода Delete
+        delete_url = base_url + delete_resource + key
+        print(delete_url)
+        json_for_delete_new_location = {
+            "place_id": place_id
+        }
+        result_delete = Http_methods.delete(delete_url, json_for_delete_new_location)
+        print(result_delete.text)
+        return result_delete
