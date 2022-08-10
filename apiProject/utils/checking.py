1b60c4dc0b353d547e7c15fa726a980327ff686d
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-08-10 23:13:05 +0300

    Create checking.py

---
+++ apiProject/utils/checking.py
@@ -0,0 +1,25 @@
+"""Методы для проверки ответов наших запросов"""
+import json
+
+from requests import Response
+import json
+
+class Checking():
+
+
+    """Метод для проверки статус кода"""
+    @staticmethod
+    def check_status_code(response: Response, status_code):
+        assert status_code == response.status_code
+        if response.status_code == status_code:
+            print("Успешно!!! Статус код = " + str(response.status_code))
+        else:
+            print("Провал!!! Статус код = " + str(response.status_code))
+
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
