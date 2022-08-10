18412f56b0cc8264df052a63d7af647699fe1213
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-08-10 23:10:30 +0300

    Update test_google_maps_api.py

--- apiProject/tests/test_google_maps_api.py
+++ apiProject/tests/test_google_maps_api.py
@@ -1,8 +1,10 @@
-from requests import Response
+import json
 
+from requests import Response
+from utils.checking import Checking
 from utils.api import Google_maps_api
 
-"""Создание, изменение и удаление новой локации."""
+"""Создание, изменение и удаление новой локации"""
 
 class Test_create_place():
 
@@ -12,19 +14,34 @@ class Test_create_place():
         result_post: Response = Google_maps_api.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, list(json.loads(result_post.text)))
+        # token = json.loads(result_post.text)
+        # print(list(token))
 
         print("Метод GET POST")
         result_get: Response = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, list(json.loads(result_get.text)))
 
         print("Метод PUT")
         result_put: Response = Google_maps_api.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
 
         print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, list(json.loads(result_get.text)))
 
         print("Метод DELETE")
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
 
         print("Метод GET DELETE")
         result_get: Response = Google_maps_api.get_new_place(place_id)
-    
+        Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+
+        print("Тестирование создания, изменения и удаления новой локации прошло успешно!!!")
