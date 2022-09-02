2257cb9122247e454f12cb6cc82840626725f7f2
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-09-02 21:24:26 +0300

    Update test_google_maps_api.py

--- apiProject/tests/test_google_maps_api.py
+++ apiProject/tests/test_google_maps_api.py
@@ -16,30 +16,38 @@ class Test_create_place():
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
         Checking.check_json_token(result_post, list(json.loads(result_post.text)))
+        token = json.loads(result_post.text)
+        Checking.check_json_value(result_post, 'status', 'OK')
 
         print("Метод GET POST")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, list(json.loads(result_get.text)))
+        Checking.check_json_value(result_get, 'address', '29, side layout, cohen 09')
 
         print("Метод PUT")
         result_put: Response = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
         Checking.check_json_token(result_put, ['msg'])
+        Checking.check_json_value(result_put, 'msg', 'Address successfully updated')
 
         print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
         Checking.check_json_token(result_get, list(json.loads(result_get.text)))
+        Checking.check_json_value(result_get, 'address', '100 Lenina street, RU')
 
         print("Метод DELETE")
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
         Checking.check_json_token(result_delete, ['status'])
+        Checking.check_json_value(result_delete, 'status', 'OK')
 
         print("Метод GET DELETE")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
         Checking.check_json_token(result_get, ['msg'])
+        Checking.check_json_search_word_in_value(result_get, 'msg', 'failed')
+
 
         print("Тестирование создания, изменения и удаления новой локации прошло успешно!!!")
