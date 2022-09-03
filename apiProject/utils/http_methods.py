f1185f32b209a54ae4146214e8bcaf0cbb46b4c1
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2022-09-03 21:20:00 +0300

    Update http_methods.py

--- apiProject/utils/http_methods.py
+++ apiProject/utils/http_methods.py
@@ -1,6 +1,8 @@
 import requests
 
-"""Список HTTP методов."""
+from utils.logger import Logger
+
+"""Список HTTP методов"""
 
 class Http_methods:
     headers = {'Content-Type': 'application/json'}
@@ -8,24 +10,31 @@ class Http_methods:
 
     @staticmethod
     def get(url):
+        Logger.add_request(url, method="GET")
         result = requests.get(url, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
 
     @staticmethod
     def post(url, body):
+        Logger.add_request(url, method="POST")
         result = requests.post(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
 
     @staticmethod
     def put(url, body):
+        Logger.add_request(url, method="{PU}T")
         result = requests.put(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
 
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, method="DELETE")
         result = requests.delete(url, json=body, headers=Http_methods.headers, cookies=Http_methods.cookie)
+        Logger.add_response(result)
         return result
-    
